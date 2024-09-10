edf3bc11d923b8291efadb460a47ea9dea5647a6
savasduzgun <savasdzgn@gmail.com>
2024-09-10 22:25:49 +0300

    User classı claims deki user la çakışmasın diye AppUser olarak değiştirildi.

---
+++ NTierArchitecture.Entities/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NTierArchitecture.Entities.Models
+{
+    public sealed class AppUser : IdentityUser<Guid>
+    {
+        public string Name { get; set; }
+        public string Lastame { get; set; }
+    }
+}
