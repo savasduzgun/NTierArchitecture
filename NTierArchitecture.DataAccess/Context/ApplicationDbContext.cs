48f106770cc336dcd7dbd5a5f991ccc3ac15d743
savasduzgun <savasdzgn@gmail.com>
2024-09-12 21:20:47 +0300

    ApplicationDbContext classı eklendi sqlserver ve tools paketleri kuruldu.

---
+++ NTierArchitecture.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using NTierArchitecture.Entities.Models;
+
+namespace NTierArchitecture.DataAccess.Context
+{
+    internal sealed class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+
+    }
+}
