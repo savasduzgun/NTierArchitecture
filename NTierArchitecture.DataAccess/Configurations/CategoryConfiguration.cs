e09390d794496a8f1392f82e80b7af99d5a912ed
savasduzgun <savasdzgn@gmail.com>
2024-09-13 17:15:15 +0300

    CategoryConfiguration class ı eklendi.

---
+++ NTierArchitecture.DataAccess/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NTierArchitecture.Entities.Models;
+
+namespace NTierArchitecture.DataAccess.Configurations
+{
+    internal sealed class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name).HasColumnType("varchar(100)");
+        }
+    }
+}
