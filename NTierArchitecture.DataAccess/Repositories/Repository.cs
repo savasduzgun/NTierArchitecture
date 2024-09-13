ac0253951a5e5dcc46de8008dcc009d85409d5b4
savasduzgun <savasdzgn@gmail.com>
2024-09-13 20:40:09 +0300

    Repository Pattern generic yapısı kuruldu.

---
+++ NTierArchitecture.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NTierArchitecture.DataAccess.Context;
+using NTierArchitecture.Entities.Repositories;
+
+namespace NTierArchitecture.DataAccess.Repositories
+{
+    internal sealed class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _context.Set<T>().AddAsync(entity, cancellationToken);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _context.Set<T>().AsNoTracking().AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(System.Linq.Expressions.Expression<Func<T, bool>> expression, CancellationToken cancellationToken)
+        {
+            return await _context.Set<T>().Where(expression).FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public IQueryable<T> GetWhere(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().AsNoTracking().Where(expression).AsQueryable();
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);
+        }
+    }
+}
