3664fcf0def0e1f677eb54132c531bc43fcf478a
savasduzgun <savasdzgn@gmail.com>
2024-09-13 20:48:12 +0300

    ProductRepository oluşturuldu.

---
+++ NTierArchitecture.DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using NTierArchitecture.DataAccess.Context;
+using NTierArchitecture.Entities.Models;
+using NTierArchitecture.Entities.Repositories;
+
+namespace NTierArchitecture.DataAccess.Repositories
+{
+    internal sealed class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
