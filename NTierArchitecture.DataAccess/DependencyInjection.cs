46649777fcb014d73452eeca8bc54fca44ce4166
savasduzgun <savasdzgn@gmail.com>
2024-09-14 11:57:39 +0300

    DataAccess katmanına identity paketi kuruldu.

---
+++ NTierArchitecture.DataAccess/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NTierArchitecture.DataAccess.Context;
+
+namespace NTierArchitecture.DataAccess
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration) 
+        {
+            string connectionString = configuration.GetConnectionString("SqlServer");
+            services.AddDbContext<ApplicationDbContext>(options =>
+            { 
+                options.UseSqlServer(connectionString);
+            });
+            return services;
+        }
+    }
+}
