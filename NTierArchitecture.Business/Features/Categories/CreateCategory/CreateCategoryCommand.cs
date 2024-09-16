022f19bc95d367613c230205d9db02f0eec13657
savasduzgun <savasdzgn@gmail.com>
2024-09-16 23:44:37 +0300

    classlalarn bir defaya mahsus instance ı üretilen ikincisinde değerin değiştirilemez olarak kullanılmasını sağlayan request ve dto larda kullanışlı oaln bir yapı olan record yazıldı.

--- NTierArchitecture.Business/Features/Categories/CreateCategory/CreateCategoryCommand.cs
+++ NTierArchitecture.Business/Features/Categories/CreateCategory/CreateCategoryCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace NTierArchitecture.Business.Features.Categories.CreateCategory
 {
-    internal class CreateCategoryCommand
+    public sealed record CreateCategoryCommand(
+        string Name) : IRequest;
+
+    internal sealed class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand>
     {
+        public async Task Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
