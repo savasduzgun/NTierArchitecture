bbad98f3bdc0fa2fc9d9fb080f576fa8d9d8f841
savasduzgun <savasdzgn@gmail.com>
2024-09-19 22:40:18 +0300

    IRepository e AnyAsync metodu eklendi.

--- NTierArchitecture.Entities/Repositories/IRepository.cs
+++ NTierArchitecture.Entities/Repositories/IRepository.cs
@@ -7,8 +7,9 @@ namespace NTierArchitecture.Entities.Repositories
         Task AddAsync(T entity, CancellationToken cancellationToken = default);
         void Update(T entity);
         void Remove(T entity);
-        Task<T> GetByIdAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken);
+        Task<T> GetByIdAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
         IQueryable<T> GetAll();
         IQueryable<T> GetWhere(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
     }
 }
