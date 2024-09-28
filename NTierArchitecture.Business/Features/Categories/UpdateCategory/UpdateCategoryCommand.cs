2a53e67a3a2e12f660556195e242ea21a77e1bd9
savasduzgun <savasdzgn@gmail.com>
2024-09-29 00:04:15 +0300

    Mevcut isim varsa hata verir.

--- NTierArchitecture.Business/Features/Categories/UpdateCategory/UpdateCategoryCommand.cs
+++ NTierArchitecture.Business/Features/Categories/UpdateCategory/UpdateCategoryCommand.cs
@@ -25,6 +25,16 @@ namespace NTierArchitecture.Business.Features.Categories.UpdateCategory
             {
                 throw new ArgumentException("Kategori bulunamadı!");
             }
+
+            if (category.Name != request.Name) 
+            {
+                var isCategoryNameExists = await _categoryRepository.AnyAsync(p => p.Name == request.Name, cancellationToken);
+
+                if (isCategoryNameExists)
+                {
+                    throw new ArgumentException("Bu kategori adı daha önce oluşturulmuş!");
+                }
+            }
         }
     }
 }
