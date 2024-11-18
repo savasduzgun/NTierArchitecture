717ee4a58d2a0118066c7fcd8155e9cdef62b131
savasduzgun <savasdzgn@gmail.com>
2024-11-18 23:00:43 +0300

    GetProductsQueryHandler class ı yazıldı.

--- NTierArchitecture.Business/Features/Products/GetProducts/GetProductsQueryHandler.cs
+++ NTierArchitecture.Business/Features/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NTierArchitecture.Entities.Models;
+using NTierArchitecture.Entities.Repositories;
 
 namespace NTierArchitecture.Business.Features.Products.GetProducts
 {
-    internal class GetProductsQueryHandler
+    internal sealed class GetProductsQueryHandler : IRequestHandler<GetProductsQuery,
+        List<Product>>
     {
+        private readonly IProductRepository _productRepository;
+        public GetProductsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<List<Product>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetAll().OrderBy(p => p.Name).ToListAsync(cancellationToken);
+        }
     }
 }
