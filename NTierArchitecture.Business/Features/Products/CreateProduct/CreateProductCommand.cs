1e252cdfe86d5d0b666297df7be2548c9bbad35e
savasduzgun <savasdzgn@gmail.com>
2024-11-18 23:04:06 +0300

    CreateProductCommand class ı yazıldı.

--- NTierArchitecture.Business/Features/Products/CreateProduct/CreateProductCommand.cs
+++ NTierArchitecture.Business/Features/Products/CreateProduct/CreateProductCommand.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace NTierArchitecture.Business.Features.Products.CreateProduct
 {
-    internal class CreateProductCommand
-    {
-    }
+    public sealed record CreateProductCommand(
+         string Name) : IRequest;
 }
