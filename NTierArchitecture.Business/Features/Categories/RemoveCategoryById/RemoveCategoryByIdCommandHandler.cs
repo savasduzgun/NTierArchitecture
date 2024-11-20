31cc0493535ef979ebd2e5bd773b441f0b55e13c
savasduzgun <savasdzgn@gmail.com>
2024-11-20 23:30:02 +0300

    RemoveCategoryByIdCommandHandler class ı yazıldı.

--- NTierArchitecture.Business/Features/Categories/RemoveCategoryById/RemoveCategoryByIdCommandHandler.cs
+++ NTierArchitecture.Business/Features/Categories/RemoveCategoryById/RemoveCategoryByIdCommandHandler.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using NTierArchitecture.Entities.Models;
+using NTierArchitecture.Entities.Repositories;
 
 namespace NTierArchitecture.Business.Features.Categories.RemoveCategoryById
 {
-    internal class RemoveCategoryByIdCommandHandler
+    internal sealed class RemoveCategoryByIdCommandHandler : IRequestHandler<RemoveCategoryByIdCommand>
     {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public RemoveCategoryByIdCommandHandler(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+        {
+            _categoryRepository = categoryRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(RemoveCategoryByIdCommand request, CancellationToken cancellationToken)
+        {
+            Category category = await _categoryRepository.GetByIdAsync(p => p.Id == request.Id, cancellationToken);
+            if (category is null)
+            {
+                throw new ArgumentException("Kategori bulunamadı!");
+            }
+
+            _categoryRepository.Remove(category);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
     }
 }
