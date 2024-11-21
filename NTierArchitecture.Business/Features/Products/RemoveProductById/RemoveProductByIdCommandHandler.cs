210d0d806be7b0d1f3a7c3d35f5703a6924b73cb
savasduzgun <savasdzgn@gmail.com>
2024-11-21 18:49:55 +0300

    RemoveProductByIdCommandHandler

--- NTierArchitecture.Business/Features/Products/RemoveProductById/RemoveProductByIdCommandHandler.cs
+++ NTierArchitecture.Business/Features/Products/RemoveProductById/RemoveProductByIdCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MediatR;
+using NTierArchitecture.Entities.Models;
+using NTierArchitecture.Entities.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,26 @@ using System.Threading.Tasks;
 
 namespace NTierArchitecture.Business.Features.Products.RemoveProductById
 {
-    internal class RemoveProductByIdCommandHandler
+    internal sealed class RemoveProductByIdCommandHandler : IRequestHandler<RemoveProductByIdCommand>
     {
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public RemoveProductByIdCommandHandler(IProductRepository productRepository, IUnitOfWork unitOfWork)
+        {
+            _productRepository = productRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(RemoveProductByIdCommand request, CancellationToken cancellationToken)
+        {
+            Product product = await _productRepository.GetByIdAsync(p => p.Id == request.Id, cancellationToken);
+            if (product is null)
+            {
+                throw new ArgumentException("Ürün bulunamadı!");
+            }
+
+            _productRepository.Remove(product);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
     }
 }
