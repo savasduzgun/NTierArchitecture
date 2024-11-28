246983ed45427a70ee96fb42a4f7ba8cae9ec562
savasduzgun <savasdzgn@gmail.com>
2024-11-28 22:25:56 +0300

    ValidationBehavior class ı yazıldı.

--- NTierArchitecture.Business/Behaviors/ValidationBehavior.cs
+++ NTierArchitecture.Business/Behaviors/ValidationBehavior.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
 
 namespace NTierArchitecture.Business.Behaviors
 {
-    internal class ValidationBehavior
+    public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : class, IRequest<TResponse>, IRequest
     {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!_validators.Any()) 
+            {
+                return await next();
+            }
+
+            //ValidationContext üzerinden validation kuralları denetlenir
+            var context = new ValidationContext<TRequest>(request);
+
+            //yazılan kurallara göre bir hata varsa errorDictionary e set edilir
+            var errorDictionary = _validators
+                .Select(s => s.Validate(context))
+                .SelectMany(s => s.Errors)
+                .Where(s => s != null)
+                .GroupBy(
+                s => s.PropertyName,
+                s => s.ErrorMessage, (propertyName, errorMessage) => new 
+                {
+                    Key = propertyName,
+                    Values = errorMessage.Distinct().ToArray()
+                })
+                .ToDictionary(s => s.Key, s => s.Values[0]);
+
+            //errorDictionary de hata varsa validasyon hatası olarak ekrana bas yoksa devam et
+            if (errorDictionary.Any())
+            {
+                var errors = errorDictionary.Select(s => new ValidationFailure
+                {
+                    PropertyName = s.Value,
+                    ErrorCode = s.Key
+                });
+
+                throw new ValidationException(errors);
+            }
+
+            return await next();
+        }
     }
 }
