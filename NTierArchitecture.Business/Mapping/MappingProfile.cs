0255595bf08ca03bcb1f0f9f59217bf29a8911b4
savasduzgun <savasdzgn@gmail.com>
2024-11-30 23:23:11 +0300

    MappingProfile class ı yazıldı.

--- NTierArchitecture.Business/Mapping/MappingProfile.cs
+++ NTierArchitecture.Business/Mapping/MappingProfile.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using NTierArchitecture.Business.Features.Categories.CreateCategory;
+using NTierArchitecture.Business.Features.Categories.UpdateCategory;
+using NTierArchitecture.Business.Features.Products.CreateProduct;
+using NTierArchitecture.Business.Features.Products.UpdateProduct;
+using NTierArchitecture.Entities.Models;
 
 namespace NTierArchitecture.Business.Mapping
 {
-    internal class MappingProfile
+    internal sealed class MappingProfile : Profile
     {
+        //hangi class ın hangi class a convertleneceği belirtildi.
+        public MappingProfile()
+        {
+            CreateMap<CreateProductCommand, Product>();
+            CreateMap<UpdateProductCommand, Product>();
+            CreateMap<CreateCategoryCommand, Category>();
+            CreateMap<UpdateCategoryCommand, Category>();
+        }
     }
 }
