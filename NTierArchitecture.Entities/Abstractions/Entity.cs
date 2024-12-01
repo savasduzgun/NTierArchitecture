e788ce41a71c150b55f44e06acd2ec78615a7c3a
savasduzgun <savasdzgn@gmail.com>
2024-12-01 21:50:45 +0300

    Entity class ı yazıldı.

--- NTierArchitecture.Entities/Abstractions/Entity.cs
+++ NTierArchitecture.Entities/Abstractions/Entity.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NTierArchitecture.Entities.Abstractions
+﻿namespace NTierArchitecture.Entities.Abstractions
 {
-    internal class Entity
+    internal abstract class Entity
     {
+        public Guid Id { get; set; }
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
     }
 }
