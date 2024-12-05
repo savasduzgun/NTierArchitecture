4f73f8b584f476c5ef0cd51e69355d4ccc0e4352
SavasDuzgun <savasdzgn@gmail.com>
2024-12-05 14:47:19 +0300

    ApiController eklendi.

---
+++ NTierArchitecture.WebApi/Abstractions/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NTierArchitecture.WebApi.Abstractions
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+    }
+}
