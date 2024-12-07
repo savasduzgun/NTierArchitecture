ebcb7fdbefc2b122512bc7ff2ea0e6cc3e11664a
SavasDuzgun <savasdzgn@gmail.com>
2024-12-07 15:17:47 +0300

    ProductsController eklendi.

---
+++ NTierArchitecture.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NTierArchitecture.WebApi.Abstractions;
+
+namespace NTierArchitecture.WebApi.Controllers
+{
+    public sealed class ProductsController : ApiController
+    {
+        
+    }
+}
