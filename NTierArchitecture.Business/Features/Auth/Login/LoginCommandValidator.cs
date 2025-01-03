100affeea059694eaef70e47b754a83800a7e69f
SavasDuzgun <savasdzgn@gmail.com>
2025-01-03 16:24:23 +0300

    LoginCommandValidator class ı yazıldı.

--- NTierArchitecture.Business/Features/Auth/Login/LoginCommandValidator.cs
+++ NTierArchitecture.Business/Features/Auth/Login/LoginCommandValidator.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
 namespace NTierArchitecture.Business.Features.Auth.Login
 {
-    internal class LoginCommandValidator
+    public sealed class LoginCommandValidator : AbstractValidator<LoginCommand>
     {
+        public LoginCommandValidator()
+        {
+            RuleFor(p => p.UserNameOrEmail).NotEmpty().WithMessage("Kullanıcı adı boş olamaz");
+            RuleFor(p => p.UserNameOrEmail).NotNull().WithMessage("Kullanıcı adı boş olamaz");
+            RuleFor(p => p.UserNameOrEmail).MinimumLength(3).WithMessage("Kullanıcı adı en az 3 karakter olmalıdır");
+            RuleFor(p => p.Password).NotEmpty().WithMessage("Şifre boş olamaz");
+            RuleFor(p => p.Password).NotNull().WithMessage("Şifre boş olamaz");
+            RuleFor(p => p.Password).Matches("[A-Z]").WithMessage("Şifre en az 1 adet büyük harf içermelidir!");
+            RuleFor(p => p.Password).Matches("[a-z]").WithMessage("Şifre en az 1 adet küçük harf içermelidir!");
+            RuleFor(p => p.Password).Matches("[0-9]").WithMessage("Şifre en az 1 adet rakam içermelidir!");
+            RuleFor(p => p.Password).Matches("[^a-zA-Z0-9]").WithMessage("Şifre en az 1 adet özel karakter içermelidir!");
+        }
     }
 }
